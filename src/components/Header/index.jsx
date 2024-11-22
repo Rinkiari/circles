6b2ab97e96e3d28822431a2b378c631551d04892
Rinkiari <st112446@student.spbu.ru>
2024-11-22 05:26:42 +0300

    цвет хедера в зависимости от текущей страницы через хук useLocation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import styles from './Header.module.scss';
 import acc_icon from './account_icon.png';
 
@@ -7,10 +8,14 @@ import Search from '../Search';
 
 const Header = () => {
   const [isLogged, setIsLogged] = useState(true);
+  const location = useLocation();
+  console.log(`location: ${location.pathname}`);
+
+  const pageClass = location.pathname === '/' ? 'home' : location.pathname.slice(1);
 
   return (
     <div className={styles.root}>
-      <div className={styles.panel_container}>
+      <div className={`${styles.panel_container} ${styles[pageClass]}`}>
         <div className={styles.left_side}>
           <Link to="/">
             <h1>C I R C L E S</h1>
