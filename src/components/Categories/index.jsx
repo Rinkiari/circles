566767aba516058e17562cadbd69a0d21cb07b26
Rinkiari <st112446@student.spbu.ru>
2024-11-29 01:17:35 +0300

    фикс расположения категорий, фильтрация ивентов (запрос на бек)

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -9,16 +9,14 @@ import dots from './assets/dots_icon.svg';
 
 export const svg_iconsArr = [brush, ball, show, leafs, games, kino, dots];
 
-const Categories = ({ value, onClickCategory }) => {
-  const categoriesArr = ['ТВОРЧЕСТВО', 'СПОРТ', 'ШОУ', 'ПРИРОДА', 'ИГРЫ', 'КИНО', ''];
-
+const Categories = ({ categoriesArr, selectedCategories, onToggleCategory }) => {
   return (
     <div className={styles.ctg_container}>
       {categoriesArr.map((el, i) => (
         <button
-          className={`${styles.knopochka} ${value === i ? styles.active : ''}`}
+          className={`${styles.knopochka} ${selectedCategories.includes(i) ? styles.active : ''}`}
           key={i}
-          onClick={() => onClickCategory(i)}>
+          onClick={() => onToggleCategory(i)}>
           <img src={svg_iconsArr[i]} alt="icon" className={styles.icon} />
           {el}
         </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,60 @@ import Categories from '../components/Categories';
 import EventCard from '../components/EventCard';
 
 const Home = () => {
-  const [categoryId, setCategoryId] = React.useState(0); //состояние категорий
-  const [events, setEvent] = React.useState([]);
+  const categoriesArr = ['ТВОРЧЕСТВО', 'СПОРТ', 'ШОУ', 'ПРИРОДА', 'ИГРЫ', 'КИНО', ''];
+  const [selectedCategories, setSelectedCategories] = React.useState([]); //состояние категорий
+  const [events, setEvents] = React.useState([]);
+
+  const handleToggleCategory = (categoryIndex) => {
+    setSelectedCategories(
+      (prev) =>
+        prev.includes(categoryIndex)
+          ? prev.filter((id) => id !== categoryIndex) // убираем категорию, если она уже выбрана
+          : [...prev, categoryIndex], // добавляем категорию, если она не выбрана
+    );
+  };
+
+  const selectedCategoryNames = selectedCategories.map((index) => categoriesArr[index]);
 
   React.useEffect(() => {
-    fetch('https://e895c70e3c56e1a7.mokky.dev/events')
-      .then((res) => res.json())
-      .then((jsonRes) => {
-        setEvent(jsonRes);
-      });
-  }, []);
+    const fetchEvents = async () => {
+      try {
+        const url =
+          selectedCategoryNames.length > 0
+            ? `https://e895c70e3c56e1a7.mokky.dev/events?categories=*${encodeURIComponent(
+                selectedCategoryNames.join(','),
+              )}`
+            : `https://e895c70e3c56e1a7.mokky.dev/events`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+      }
+    };
+
+    fetchEvents();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedCategories]);
+
+  // OLD FETCH
+  // React.useEffect(() => {
+  //   fetch('https://e895c70e3c56e1a7.mokky.dev/events')
+  //     .then((res) => res.json())
+  //     .then((jsonRes) => {
+  //       setEvent(jsonRes);
+  //     });
+  // }, []);
 
   return (
     <>
       <Header />
-      <Categories value={categoryId} onClickCategory={(i) => setCategoryId(i)} />
+      <Categories
+        categoriesArr={categoriesArr}
+        selectedCategories={selectedCategories}
+        onToggleCategory={handleToggleCategory}
+      />
       <div className="event_container">
         {events.map((obj) => (
           <EventCard key={obj.id} {...obj} />
