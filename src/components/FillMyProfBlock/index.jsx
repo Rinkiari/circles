2c2be85a3379c73640fe856c2f17fd107332ec54
Rinkiari <st112446@student.spbu.ru>
2024-12-03 05:32:48 +0300

    fillmyprofile page(2/3 блока); POST fetch формы to backend

--- src/components/FillMyProfBlock/index.jsx
+++ src/components/FillMyProfBlock/index.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-import styles from "./FillMyProfBlock.module.scss";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './FillMyProfBlock.module.scss';
 
-import ProgressLine from "../ProgressLine";
+import ProgressLine from '../ProgressLine';
 
 const FillMyProfileBlock = () => {
+  const navigate = useNavigate();
   const [step, setStep] = React.useState(1);
   const [formValues, setFormValues] = React.useState({
-    name: "",
-    surname: "",
-    gender: "",
-    date_of_birth: "",
-    city: "",
+    name: '',
+    surname: '',
+    gender: '',
+    date_of_birth: '',
+    city: '',
+    about: '',
   });
 
   // mассив для генерации placeholder'ов
   const infoArr = [
-    { label: "Имя", key: "name" },
-    { label: "Фамилия", key: "surname" },
-    { label: "Пол", key: "gender" },
-    { label: "День рождения", key: "date_of_birth" },
-    { label: "Город", key: "city" },
+    { label: 'Имя', key: 'name' },
+    { label: 'Фамилия', key: 'surname' },
+    { label: 'Пол', key: 'gender' },
+    { label: 'День рождения', key: 'date_of_birth' },
+    { label: 'Город', key: 'city' },
   ];
 
   const handleInputChange = (key, value) => {
@@ -29,37 +32,84 @@ const FillMyProfileBlock = () => {
     }));
   };
 
-  const handleNextStep = () => {
-    const isFormValid = Object.values(formValues).every(
-      (value) => value.trim() !== "",
-    );
+  const handleNextStep = async () => {
+    let fieldsToCheck = [];
+
+    if (step === 1) {
+      fieldsToCheck = ['name', 'surname', 'gender', 'date_of_birth', 'city'];
+    } else if (step === 2) {
+      fieldsToCheck = ['about'];
+    }
+
+    const isFormValid = fieldsToCheck.every((field) => formValues[field].trim() !== '');
+
     if (!isFormValid) {
-      alert("Пожалуйста, заполните все поля перед продолжением!");
+      alert('Пожалуйста, заполните все поля перед продолжением!');
       return;
     }
-    setStep((prev) => Math.min(3, prev + 1));
+
+    if (step === 3) {
+      try {
+        // отправка данных на сервер
+        const response = await fetch('https://e895c70e3c56e1a7.mokky.dev/formtest', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formValues),
+        });
+
+        if (!response.ok) {
+          throw new Error('Ошибка отправки данных.');
+        }
+
+        alert('Данные успешно отправлены!');
+        navigate('/myprofile');
+      } catch (error) {
+        alert(`Ошибка: ${error.message}`);
+      }
+    } else {
+      setStep((prev) => Math.min(3, prev + 1));
+    }
   };
 
   return (
     <div className={styles.main_container}>
       <div className={styles.top_inner_cont}>
-        <h2>ОБЩАЯ ИНФОРМАЦИЯ</h2>
-        <p onClick={handleNextStep}>Далее</p>
+        <h2>
+          {step === 1 && 'ОБЩАЯ ИНФОРМАЦИЯ'}
+          {step === 2 && 'О СЕБЕ'}
+          {step === 3 && 'ВАШИ ИНТЕРЕСЫ'}
+        </h2>
+        <p onClick={handleNextStep}>{step === 3 ? 'Завершить' : 'Далее'}</p>
       </div>
       <ProgressLine currentStep={step} />
       <div className={styles.bottom_inner_cont}>
         <div className={styles.general_info_box}>
           <form>
-            {infoArr.map(({ label, key }) => (
-              <input
-                required
-                className={styles.inputek}
-                key={key}
-                placeholder={label}
-                value={formValues[key] || ""}
-                onChange={(e) => handleInputChange(key, e.target.value)}
-              />
-            ))}
+            {step === 1 &&
+              infoArr.map(({ label, key }) => (
+                <input
+                  required
+                  className={styles.inputek}
+                  key={key}
+                  placeholder={label}
+                  value={formValues[key] || ''}
+                  onChange={(e) => handleInputChange(key, e.target.value)}
+                />
+              ))}
+            {step === 2 && (
+              <>
+                <h4>Черканите пару строк о своей многосторонней личности!</h4>
+                <textarea
+                  required
+                  className={styles.textarea}
+                  placeholder="Расскажите о себе"
+                  value={formValues.about || ''}
+                  onChange={(e) => handleInputChange('about', e.target.value)}
+                />
+              </>
+            )}
           </form>
         </div>
       </div>
