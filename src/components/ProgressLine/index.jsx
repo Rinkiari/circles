e28f213c413ae4cf37fee5736ef7e310b2120785
Rinkiari <st112446@student.spbu.ru>
2025-01-21 04:20:15 +0300

    +верстка компонента заявок на странице ивента

--- src/components/ProgressLine/index.jsx
+++ src/components/ProgressLine/index.jsx
@@ -1,20 +1,35 @@
-import React from "react";
-import styles from "./ProgressLine.module.scss";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import styles from './ProgressLine.module.scss';
 
 const ProgressLine = ({ currentStep }) => {
+  const location = useLocation();
+
   return (
     <div className={styles.container}>
       <hr
         className={styles.progressLine}
         style={{
           background: `linear-gradient(
-      to right, 
-      ${currentStep === 1 ? "#A7DBE9" : "#CBCBCB"} 0%, 
-      ${currentStep === 1 ? "#A7DBE9" : "#CBCBCB"} 33%, 
-      ${currentStep === 2 ? "#FFDB85" : "#CBCBCB"} 33%, 
-      ${currentStep === 2 ? "#FFDB85" : "#CBCBCB"} 66%, 
-      ${currentStep === 3 ? "#FF9681" : "#CBCBCB"} 66%, 
-      ${currentStep === 3 ? "#FF9681" : "#CBCBCB"} 100%
+      to right,
+      ${currentStep === 1 ? '#A7DBE9' : '#CBCBCB'} 0%, 
+      ${currentStep === 1 ? '#A7DBE9' : '#CBCBCB'} 33%, 
+      ${currentStep === 2 ? '#FFDB85' : '#CBCBCB'} 33%, 
+      ${currentStep === 2 ? '#FFDB85' : '#CBCBCB'} 66%, 
+      ${
+        location.pathname.startsWith('/event')
+          ? '#FFDB85'
+          : currentStep === 3
+          ? '#FF9681' // Стандартный цвет для третьего шага
+          : '#CBCBCB' // Цвет, если шаг ещё не активен
+      } 66%, 
+      ${
+        location.pathname.startsWith('/event')
+          ? '#FFDB85'
+          : currentStep === 3
+          ? '#FF9681'
+          : '#CBCBCB'
+      } 100%
     )`,
         }}
       />
--- src/components/RequestsBlock/index.jsx
+++ src/components/RequestsBlock/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './RequestsBlock.module.scss';
 
-const RequestsBlock = ({ reqUsers }) => {
+import ProgressLine from '../ProgressLine';
+
+const RequestsBlock = ({ eventName, reqUsers }) => {
   return (
     <div className={styles.global_cont}>
+      <div className={styles.cont_h2}>
+        <h2>{eventName} - заявки</h2>
+      </div>
+      <div className={styles.progressbar}>
+        <p className={styles.text_end}>Завершить</p>
+        <ProgressLine currentStep={3} />
+      </div>
       <div className={styles.users_cont}>
         {reqUsers.map((us) => (
           <div className={styles.user_wrapper} key={us.id}>
